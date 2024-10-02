98a4da7de62a21b86baa9aed89ee90881ef385e9
arielpotolski <arielpotolskie@gmail.com>
2024-10-02 19:05:28 +0200

    Create repo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/who-we-are">Who We Are</Link></li>
+                <li><Link to="/what-we-do">What We Do</Link></li>
+                <li><Link to="/inspiration">Inspiration</Link></li>
+                <li><Link to="/get-in-touch">Get In Touch</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
