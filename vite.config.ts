dc56a79a56d71ccad0788b40565e8f16a7cc6556
Ariel Potolski Eilat <arielpotolskie@gmail.com>
2024-11-01 04:35:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/niceshodz/src/components/Home.tsx"
+  base: "/niceshodz/src/components/Home"
 })
